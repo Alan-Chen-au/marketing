13072521e1d2fa009e62ca63ed0b7ad31198e9e7
Alan Chen <achen@staff.onthenet.com.au>
2021-01-18 09:46:07 +1100

    refactor EmailWorker.java

--- src/main/java/exchange/pi/marketing/email/EmailWorker.java
+++ src/main/java/exchange/pi/marketing/email/EmailWorker.java
@@ -22,6 +22,11 @@ public class EmailWorker {
 	
 	private static Gson gson = new Gson(); 
 	
+	private static String TITLE_REGEX 		= "\\{\\{TITLE\\}\\}"; 
+	private static String FIRST_NAME_REGEX 	= "\\{\\{FIRST_NAME\\}\\}"; 
+	private static String LAST_NAME_REGEX 	= "\\{\\{LAST_NAME\\}\\}"; 
+	private static String TODAY_REGEX 		= "\\{\\{TODAY\\}\\}"; 
+	
 	public static void generateEmailContent(String emailTempFile, String customerFile, String outputDir, String errorFile) throws JsonIOException, IOException {
 		
 		// error handling --> make sure input file names are correct. 
@@ -42,7 +47,7 @@ public class EmailWorker {
         // read customers file and do verify
         List<String> customerStr = FileUtils.readCustomerFile(customerFile); 
         if (customerStr == null || customerStr.size() <= 1) {
-        	throw new RuntimeException("There is no customer info in the file.");  
+        	throw new RuntimeException("There is no customer in the file : " + customerFile);  
         }
         
         // remove the title line 
@@ -96,10 +101,10 @@ public class EmailWorker {
 	
 	public static Email format(Customer customer, final String emailTemplateStr, String today) {
 		String content = emailTemplateStr;   
-		content = content.replaceAll("\\{\\{TITLE\\}\\}", customer.getTitle()); 
-		content = content.replaceAll("\\{\\{FIRST_NAME\\}\\}", customer.getFirstName()); 
-		content = content.replaceAll("\\{\\{LAST_NAME\\}\\}", customer.getLastName()); 
-		content = content.replaceAll("\\{\\{TODAY\\}\\}", today); 
+		content = content.replaceAll(TITLE_REGEX, customer.getTitle()); 
+		content = content.replaceAll(FIRST_NAME_REGEX, customer.getFirstName()); 
+		content = content.replaceAll(LAST_NAME_REGEX, customer.getLastName()); 
+		content = content.replaceAll(TODAY_REGEX, today); 
 		
 		Email email = gson.fromJson(content, Email.class);
 		email.setTo(customer.getEmail());
